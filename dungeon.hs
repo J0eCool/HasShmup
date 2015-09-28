58a08b4ee4310945d0e00c950bb19480f0ca1fd7
J0eCool <count.j0ecool@gmail.com>
2015-09-27 22:36:57 -0700

    Remove unused shape functions, rename Map to Dungeon, add helper functions

--- dungeon.hs
+++ dungeon.hs
@@ -1,3 +1,5 @@
+import Data.Maybe
+
 data Point = Point
     { x :: Int
     , y :: Int
@@ -16,22 +18,14 @@ data Shape = Rect
     { pos :: Point
     , size :: Point
     }
-    | FillRect
-    { pos :: Point
-    , size :: Point
-    }
     | Circle
-    { pos :: Point
-    , radius :: Int
-    }
-    | FillCircle
     { pos :: Point
     , radius :: Int
     }
     deriving (Show)
 
-rect x y w h = FillRect (Point x y) (Point w h)
-circle x y r = FillCircle (Point x y) r
+rect x y w h = Rect (Point x y) (Point w h)
+circle x y r = Circle (Point x y) r
 
 data Grid = Grid
     { width :: Int
@@ -43,46 +37,62 @@ data Grid = Grid
 emptyGrid w h = Grid w h []
 
 data Tile = Floor | Wall
-    deriving (Show)
+    deriving (Show, Eq)
 
-data Map = Map
+data Dungeon = Dungeon
     { tiles :: [[Tile]]
     }
     deriving (Show)
 
-emptyMap w h = Map . replicate h . replicate w $ Wall
+emptyMap w h = Dungeon . replicate h . replicate w $ Wall
 
 showTile Floor = '.'
 showTile Wall = '#'
 
-showMap (Map tiles) = concatMap showLine tiles
+dimensions llist =
+    let h = length llist
+    in if h == 0
+       then (0, 0)
+       else let w = length $ head llist
+            in (w, h)
+
+tileAt (Dungeon tiles) (Point x y)
+    | y >= h = Nothing
+    | x >= w = Nothing
+    | otherwise = Just ((tiles !! y) !! x)
+    where (w, h) = dimensions tiles
+
+isWall d p = (== Wall) . fromMaybe Wall $ tileAt d p
+
+isLit d p = True
+
+showMap (Dungeon tiles) = concatMap showLine tiles
     where showLine line = map showTile line ++ "\n"
 
-gridToMap grid = Map tiles
-    where tiles = map (\y -> map (\x -> pointFilled x y) [0..w]) [0..h]
-          pointFilled x y = if isFilled grid (Point x y) then Floor else Wall
-          w = width grid - 1
-          h = height grid - 1
+showLitMap dungeon@(Dungeon tiles) = concatMap showLine points
+    where showLine line = map showPoint line ++ "\n"
+          points = pointGrid w h
+          (w, h) = dimensions tiles
+          showPoint point
+            | not $ isLit dungeon point = ' '
+            | otherwise = fromMaybe ' ' $ tileAt dungeon point >>= (\t -> Just . showTile $ t)
+
+mapMap f llist = map (\list -> map f list) llist
+pointRange xlo xhi ylo yhi = [[Point x y | x <- [xlo..xhi]] | y <- [ylo..yhi]]
+pointGrid w h = pointRange 0 (w-1) 0 (h-1)
+
+gridToMap grid = Dungeon tiles
+    where tiles = mapMap pointFilled points
+          pointFilled p = if isFilled grid p then Floor else Wall
+          points = pointGrid w h
+          w = width grid
+          h = height grid
 
 contains :: Shape -> Point -> Bool
-contains (Rect pos size) (Point x y) =
-    inRect x y left right top bot && 
-    (elem x [left, right-1] || elem y [top, bot-1])
+contains (Rect pos size) (Point x y) = inRect x y left right top bot
     where Point left top = pos
           Point right bot = pos /+/ size
-contains (FillRect pos size) (Point x y) = inRect x y left right top bot
-    where Point left top = pos
-          Point right bot = pos /+/ size
-contains (Circle center radius) point = any almostZero [l, r]
-    where (Point dx dy) = point /-/ center
-          (dx', dy') = (absMin dx dy, absMax dx dy)
-          dx'' = fromIntegral dx'
-          dy'' = fromIntegral dy'
-          radius' = fromIntegral radius
-          cx = sqrt (radius'^2 - dx''^2)
-          l = cx - dy''
-          r = cx + dy''
-contains (FillCircle center r) point = dist <= r' - 0.5
+contains (Circle center r) point = dist <= r' - 0.5
     where diff = point /-/ center
           dist = magnitude diff
           r' = fromIntegral r
@@ -122,4 +132,4 @@ main = do
              , circle 32 8 12
              , circle 80 50 50
              ]
-    putStr . showMap . gridToMap $ grid
+    putStr . showLitMap . gridToMap $ grid
