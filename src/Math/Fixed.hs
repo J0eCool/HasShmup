4124bd24493ed7cb1e825a35199bd069be600275
J0eCool <count.j0ecool@gmail.com>
2015-11-22 02:55:56 -0800

    Add fixed-point numbers to Math

---
+++ src/Math/Fixed.hs
@@ -0,0 +1,21 @@
+module Math.Fixed where
+
+data Fixed = Fixed Integer
+    deriving (Eq)
+
+fixedDigits = 3
+fixedSize = 10 ^ fixedDigits
+fixed n = Fixed . round $ n * fromInteger fixedSize
+
+fixedHoist f (Fixed x) = Fixed $ f x
+
+instance Num Fixed where
+    Fixed x + Fixed y = Fixed $ x + y
+    Fixed x * Fixed y = Fixed $ x * y `div` fixedSize
+    fromInteger n = Fixed $ n * fixedSize
+    abs = fixedHoist abs
+    signum = fixedHoist signum
+    negate = fixedHoist negate
+
+instance Show Fixed where
+    show (Fixed x) = show (fromInteger x / fromInteger fixedSize)
